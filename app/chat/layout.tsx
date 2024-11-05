afeb9fef85b41836e54495de3539def22c1df174
Abbey.D <abbey.deng@sjfood.com>
2024-11-05 01:08:49 -0800

    feat:hello world

---
+++ app/chat/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+ <div className="bg-blue-500 p-10">{children}</div>
+  )
+}
---
+++ app/chat/page.tsx
@@ -0,0 +1,18 @@
+
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+    title: 'chat',
+
+  }
+  
+export default function Chat() {
+  return (
+    <main className="bg-pink-500 p-10">
+     <h1>chat</h1>
+     
+   
+    </main>
+  )
+}
